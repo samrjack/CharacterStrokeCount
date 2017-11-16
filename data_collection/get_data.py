be317bea965ab4761dbf1a78f5e88a7055cfe0bb
samrjack <dsiq3g@gmail.com>
2017-11-16 15:01:12 -0600

    Get_data stops crashing now

--- data_collection/get_data.py
+++ data_collection/get_data.py
@@ -1,5 +1,15 @@
 import image_maker as im
 import char_util as cu
 import download_data as d
+import time
 
-map(lambda x: im.make_char(x, 32), cu.trad_and_simp(d.get_data()))
+ac = cu.trad_and_simp(d.get_data())
+count = 0
+for c in ac[count:]:
+    print c + " " + str(count)
+    count += 1
+    im.make_char(c,32)
+    time.sleep(.01)
+
+#map(lambda x: im.make_char(x, 32), cu.trad_and_simp(d.get_data()))
+
