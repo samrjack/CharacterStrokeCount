6fa2d8b02a633bf4150ad592d1229ee2bb3dde9c
samrjack <dsiq3g@gmail.com>
2017-11-16 15:07:59 -0600

    Added a zip of pictures from 西遊記 since it took so long to correctly run.

--- data_collection/get_data.py
+++ data_collection/get_data.py
@@ -4,6 +4,9 @@ import download_data as d
 import time
 
 ac = cu.trad_and_simp(d.get_data())
+
+# Only by adding count would the program properly work
+# Don't know why, but will leave it as is for now.
 count = 0
 for c in ac[count:]:
     print c + " " + str(count)
@@ -11,5 +14,6 @@ for c in ac[count:]:
     im.make_char(c,32)
     time.sleep(.01)
 
+# Using map would crash the program for some reason
 #map(lambda x: im.make_char(x, 32), cu.trad_and_simp(d.get_data()))
 
