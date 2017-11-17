14a6c83ac887b2fc475e04c56919111d470cdb50
samrjack <dsiq3g@gmail.com>
2017-11-17 11:35:55 -0600

    Able to import photos as numpy array.

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ http://www.cnblogs.com/fengfenggirl/p/python_worddb.html - How I was able to pro
 Examples of tensorflow
 https://www.geekhub.cn/a/1455.html
 http://blog.topspeedsnail.com/archives/10897
+
+https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.linalg.svd.html
+https://sourcedexter.com/manipulating-image-pixels-with-python-scikit-image-color-schemes/
+https://superuser.com/questions/644272/how-do-i-delete-all-files-smaller-than-a-certain-size-in-all-subfolders
+
--- data_statistics/stats.py
+++ data_statistics/stats.py
@@ -1,10 +1,26 @@
 import os
 import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
+from skimage.io import imread
 from itertools import groupby
 
+def get_file_names(list_of_stroke_counts=False):
+    file_names = os.listdir("../data/")
+    if list_of_stroke_counts is False:
+        return file_names
+    desired_strokes = [str(c) for c in list_of_stroke_counts]
+    return [file_name for file_name in file_names if file_name.split(".")[0] in desired_strokes]
+
+def get_pictures_with_stroke_count(list_of_stroke_counts=[]):
+    files = get_file_names(list_of_stroke_counts)
+    return np.array(list(map(import_photo, files)))
+
+def import_photo(file_name):
+    image = imread("../data/" + file_name)
+    return image.flatten()
+
 def get_data_bar_graph():
-    files = os.listdir("../data/")
+    files = get_file_names()
     files = [c.split(".")[0] for c in files]
     files.sort()
     a = [(int(key),len(list(group))) for key, group in groupby(files)]
@@ -20,4 +36,4 @@ def get_data_bar_graph():
 
     plt.show()
     
-get_data_bar_graph()
+#get_data_bar_graph()
