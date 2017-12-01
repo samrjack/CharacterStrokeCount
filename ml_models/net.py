4f3d7814631d106065f1c2b0b0ca1baf6406a658
samrjack <dsiq3g@gmail.com>
2017-12-01 14:55:07 -0600

    Getting 9% - 10% accuracy

--- ml_models/net.py
+++ ml_models/net.py
@@ -31,17 +31,18 @@ def neural_net():
                         , random_state=42)
 
         scale_result = lambda y: y/100.0
-        scale_guess  = lambda _y: int(round(_y * 100))
+        scale_guess  = lambda _y: round(_y * 100)
 
         # Define model meta parameters
-        learning_rate = 0.1
-        num_epoch = 10
-        training_size = 1000
+        learning_rate = 0.00006
+        num_epoch = 1000
+        training_size = 100
 
         n_inputs = du.import_photo(training_data[0]).shape[0]
         n_outputs = 1
-        n_hidden_1 = 500
-        n_hidden_2 = 500
+        n_hidden_1 = 100
+        n_hidden_2 = 200
+        n_hidden_3 = 50
 
         # Build tensorflow model.
         inputs = tf.placeholder(tf.float32, [None, n_inputs], name="inputs")
@@ -53,19 +54,28 @@ def neural_net():
         weight2 = tf.Variable(tf.random_uniform([n_hidden_1, n_hidden_2], -.1, .1), name="weight2")
         biases2 = tf.Variable(tf.random_uniform([n_hidden_2], -.1, .1), name="bias2")
 
-        weight3 = tf.Variable(tf.random_uniform([n_hidden_2, n_outputs], -.1, .1), name="weight3")
-        biases3 = tf.Variable(tf.random_uniform([n_outputs], -.1, .1), name="bias3")
+        weight3 = tf.Variable(tf.random_uniform([n_hidden_2, n_hidden_3], -.1, .1), name="weight3")
+        biases3 = tf.Variable(tf.random_uniform([n_hidden_3], -.1, .1), name="bias3")
+
+        weight4 = tf.Variable(tf.random_uniform([n_hidden_3, n_outputs], -.1, .1), name="weight4")
+        biases4 = tf.Variable(tf.random_uniform([n_outputs], -.1, .1), name="bias4")
 
         u1 = tf.add(tf.matmul(inputs, weight1), biases1)
-        y1 = tf.nn.sigmoid(u1)
+        # y1 = tf.nn.sigmoid(u1)
+        y1 = tf.nn.relu_layer(x=inputs, weights=weight1, biases=biases1)
 
         u2 = tf.add(tf.matmul(u1, weight2), biases2)
-        y2 = tf.nn.sigmoid(u2)
+        # y2 = tf.nn.sigmoid(u2)
+        y2 = tf.nn.relu_layer(x=y1, weights=weight2, biases=biases2)
 
         u3 = tf.add(tf.matmul(u2, weight3), biases3)
-        output = tf.nn.sigmoid(u3)
+        # y3 = tf.nn.sigmoid(u3)
+        y3 = tf.nn.relu_layer(x=y2, weights=weight3, biases=biases3)
 
-        loss = tf.nn.l2_loss(output - labels)
+        u4 = tf.add(tf.matmul(u3, weight4), biases4)
+        output = tf.nn.sigmoid(u4)
+
+        loss = tf.nn.l2_loss(output - scale_result(labels))
         optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
 
         init = tf.global_variables_initializer()
@@ -76,31 +86,37 @@ def neural_net():
                 for epoch in range(num_epoch):
                         # Pick random samples of x to train with
                         indices = random.sample(range(len(X_train)), training_size)
-                        #indices = random.sample(range(len(X_train)), 1)
 
                         data = du.get_pictures([X_train[i] for i in indices])
-                        results = np.array([scale_result(float(y_train[i])) for i in indices])
+                        results = np.array([float(y_train[i]) for i in indices])
 
-                        _, cost, prediction = sess.run([optimizer, loss, output] 
-                                    , feed_dict={inputs: data, labels: results.reshape((len(results),1))})
+                        _, cost, prediction = sess.run([optimizer, loss, output], feed_dict={inputs: data, labels: results.reshape(-1,n_outputs)})
 
-                        print(str(epoch) + "/" + str(num_epoch) + " -- " + str(cost) + "  " + X_train[indices[0]]);
+                        weight1.eval()
+                        weight2.eval()
+                        weight3.eval()
+
+                        biases1.eval()
+                        biases2.eval()
+                        biases3.eval()
+
+                        # pred = list(map(scale_guess, prediction.reshape(-1).tolist()))
+                        # print(list(zip(pred,results.tolist())), "\n\n")
+                        print(str(epoch) + "/" + str(num_epoch) + " -- " + str(cost))
 
                 data = du.get_pictures(X_test)
-                predictions = sess.run([output], feed_dict={inputs:data})
+                predictions = sess.run(output, feed_dict={inputs:data})
 
-                print(predictions[0].tolist())
+                predList = [round(scale_guess(x)) for x in predictions.reshape(-1).tolist()]
                 
-                predList = [round(x[0]) for x in predictions[0].tolist()]
-                
-                error = y_test - predList
+                error = (np.array(y_test) - np.array(predList)).tolist()
                 error.sort()
                 percentCorrect = len([x for x in error if x == 0])/float(len(error))
 
                 values = [k for k, g in it.groupby(error)]
                 amount = list(map(len, [list(g) for k, g in it.groupby(error)]))
 
-                print('Percent error =', percentCorrect * 100, '%')
+                print('Percent correct =', percentCorrect * 100, '%')
 
                 plt.close('all')
                 plt.figure(figsize=(13,10))
@@ -113,4 +129,4 @@ def neural_net():
 
 
 if __name__ == "__main__":
-        lin_tensor()
+    neural_net()
